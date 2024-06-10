df0dace48d147c22f148421dddd78f999736cbb2
Vivek Dagar <vivekdagar2023@gmail.com>
2024-06-10 17:41:48 +0530

    added array, polynomial

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -1,22 +1,14 @@
 #include <iostream>
-#include "linked list/types/circular doubly/CDoublyLL.h"
+#include "polynomial/array/PolyArr.h"
 
 using std::cin, std::cout;
 
 // for remove, indices start from 1 onwards
 
 int main() {
-    std::vector<int> ob1 = {1, 2, 3, 4, 5};
-    CDoublyLL obj(ob1);
-    obj.insert(1, 23);
-    obj.insert(6, 23);
-    obj.remove(2);
-    obj.remove(1);
-    obj.remove(1);
-    obj.remove(1);
-    obj.remove(1);
-    obj.remove(1);
-    obj.remove(1);
-    obj.remove(1);
-    cout << obj.length();
+    PolyArr p(2);
+    p.input();
+    PolyArr p1(2);
+    p1.input();
+    cout << p.eval(1);
 }
